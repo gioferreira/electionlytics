dfe03599854ffce6ad28c1c16144f1273df47a9b
Giovani Ferreira <gioferreira@gmail.com>
2018-10-25 12:34:49 -0300

    create functions to extract 3k tweets per day given an interval

--- scripts/0 - getdata.R
+++ scripts/0 - getdata.R
@@ -10,20 +10,50 @@ setup_twitter_oauth(consumer_key = consumer_key,
                     consumer_secret = consumer_secret, 
                     access_token = access_token, 
                     access_secret = access_secret)
-
 terms <- c("#elenao", "#haddad13")
-terms_search <- paste(terms, collapse = " OR ")
+since <- "2018-10-08"
+until <- "2018-10-24"
+save_to <- "data/haddad.csv"
 
-haddad <- searchTwitter(terms_search, 
-                        n = 3000, 
-                        lang = "pt", 
-                        since = "2018-10-08",
-                        until = "2018-10-09",
-                        retryOnRateLimit = 500)
+parameters <- list("terms" = terms, 
+                   "since" = since, 
+                   "until" = until, 
+                   "save_to" = save_to)
 
-haddad <- twListToDF(haddad)
+make_params_list <- function(terms,
+                             since,
+                             until,
+                             save_to){
+  library(lubridate)
+  interv <- interval(ymd(since),ymd(until))
+  n_params <- as.period(interv) %>% day()
+  params_list <- rep(list(list()), n_params)
+  for (i in 1:n_params) {
+    params_list[[i]] <- list("terms" = terms, 
+                           "since" = since, 
+                           "until" = ymd(since) + days(i), 
+                           "save_to" = save_to)
+  }
+  return(params_list)
+}
 
-write_csv(haddad, "data/haddad.csv", append = TRUE)
+extract_tweets <- function(params) {
+  terms <- params$terms
+  since <- params$since
+  until <- params$until
+  save_to <- params$save_to
+  terms_search <- paste(terms, collapse = " OR ")
+  
+  search_result <- searchTwitter(terms_search, 
+                                 n = 3000, 
+                                 lang = "pt", 
+                                 since = since,
+                                 until = until,
+                                 retryOnRateLimit = 500)
+  
+  search_result <- twListToDF(search_result)
+  write_csv(search_result, save_to, append = TRUE)
+}
 
 terms <- c("#bolsonaro17", "#elesim")
 terms_search <- paste(terms, collapse = " OR ")
