52e64808ad45de32c1749ec075a590eb1a90532c
Giovani Ferreira <gioferreira@gmail.com>
2018-10-24 23:27:35 -0300

    read twitter apps keys

---
+++ scripts/0 - getdata.R
@@ -0,0 +1,7 @@
+library(twitteR)
+library(tidyverse)
+
+consumer_key <- read_rds("private_data/consumer_key.rds")
+consumer_secret <- read_rds("private_data/consumer_secret.rds")
+access_secret <- read_rds("private_data/access_secret.rds")
+access_secret <- read_rds("private_data/access_secret.rds")
